bf2dd169623cf9bf7e7520bcde3975d3784fd2f3
Patryk Lizoń <lizon.patryk@gmail.com>
2022-03-19 15:12:27 +0100

    cleanup boilerplate

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,10 @@
 import "./reset.css";
 import "./app.css";
-import { Counter } from "@components/Counter";
-
-import styles from "./app.module.css";
 
 export function App(): JSX.Element {
   return (
-    <div className={styles.layout}>
+    <div>
       <div>hello universe!</div>
-      <Counter />
     </div>
   );
 }
